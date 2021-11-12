9d2aa8d436b2c0b9da58092225b5019c04ddb1d6
Miroslava-Golysheva <71844666+Miroslava-Golysheva@users.noreply.github.com>
2021-11-12 12:10:12 +0300

    add main js files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chat from './components/chat'
+
+export function App() {
+    return (
+        <div className="container">
+          <Chat/>
+        </div>
+    );
+}
